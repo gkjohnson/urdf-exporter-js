05b205bc6d0e7af5b386fb4d32dc889e1abb4222
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-03-16 10:45:35 -0700

    More work

--- URDFExporter.js
+++ URDFExporter.js
@@ -4,55 +4,110 @@ class URDFExporter {
     // {
     //   name
     //   type
-    //   limits: { min, max }
+    //   limits: { lower, upper }
     //   axis
     // }
 
     // mesh func returns
     // {
-    //   name
+    //   filename
     //   file
     // }
 
-    static parse(object, robotname, jointfunc, meshfunc) {
+    static parse(object, robotname, jointfunc, meshfunc, packageprefix = 'package://') {
 
         const linksMap = new WeakMap();
         const meshesMap = new WeakMap();
+        const texMap = new WeakMap();
         const meshes = [];
+        const textures = [];
         
         let urdf = `<robot name="${robotname}">`;
 
         object.traverse( child => {
             
             const name = child.name;
-            linksMap.set(child.parent, name);
+            linksMap.set(child, name);
 
             let joint = '';
             let link = `<link name="${name}">`;
 
-            if (child instanceof THREE.Mesh) {
+            if (child instanceof THREE.Mesh && child.geometry) {
                 
-                // TODO: add visual node if mesh
+                let meshInfo = meshesMap.get(child.geometry);
+                if (!mesh) {
+                    meshInfo = meshFunc(child);
+                    meshesMap.set(child.geometry, meshInfo);
+                }
+
+                link += '<visual>';
+                {
+                    link += '<origin xyz="0 0 0" rpy="0 0 0" />';
+                    
+                    link += '<geometry>';
+                    {
+                        link += `<mesh filename="${packageprefix}/meshes/${meshInfo.filename}" />`
+                    }
+                    link += '</geometry>';
+                    
+                    link += '<material name="">';
+                    {
+                        const col = child.material.color;
+                        const rgba = `${col.r} ${col.g} ${col.b} 1`;
+
+                        link += `<color rgba="${rgba}" />`
+
+                        // TODO: add texture if needed and write texture
+                        // to array
+                    }
+                    link += '</material>';
+                }
+                link += '</visual>'
 
                 // TODO: add matching collision
-            
+
             }
             
             link += '</link>';
            
             if (child !== object) {
-                joint = `<joint name="" type="">`;
 
-                // TODO: add position
+                const jointInfo = jointfunc(child);
+                const { axis, type, name, limits } = jointInfo;
 
-                // TODO: add parent
+                joint = `<joint name="${name}" type="${type || 'fixed'}">`;
+                {
+                    // TODO: add position
+                    // TODO: get local pos and rot relative to the parent
+                    // and transformed into the URDF frame
 
-                // TODO: add child
+                    joint += `<origin xyz="" rpy="" />`;
 
-                // TODO: add axis
+                    joint += `<parent link="${linksMap.get(child.parent)}" />`;
 
-                // TODO: add limit
+                    joint += `<child link="${name}" />`;
 
+                    // TODO: Transform the axis into URDF space
+                    if (axis) {
+                        joint += `<axis xyz="${axis.x} ${axis.y} ${axis.z}" />`
+                    }
+
+
+                    // TODO: add limit
+                    if (limits) {
+
+                        let limit = '<limit ';
+                        if (limits.lower != null) {
+                            limit += `lower="${limits.lower}"`;
+                        }
+
+                        if (limits.upper != null) {
+                            limit += `upper="${limits.upper}"`;
+                        }
+
+                        limit += ' />';
+                    }
+                }
                 joint += '</joint>';
             }
 
@@ -62,7 +117,7 @@ class URDFExporter {
 
         urdf += '</robot>';
 
-        return { urdf, meshes }
+        return { urdf, meshes, textures }
 
     }
 
