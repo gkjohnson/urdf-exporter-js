0393056ec55b5c5382186753f2284cc44f873c70
Garrett Johnson <garrett.kjohnson@gmail.com>
2022-08-02 18:45:45 -0700

    Update build

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,40 @@
-const path = require('path');
-const inputPath = path.join(__dirname, `./src/index.js`);
-const outputPath = path.join(__dirname, `./umd/index.js`);
+const path = require( 'path' );
+const inputPath = path.join( __dirname, './src/index.js' );
 
-export default {
+export default [ {
 
-    input: inputPath,
-    treeshake: false,
-    external: p => /^three/.test(p),
+	input: inputPath,
+	treeshake: false,
+	external: p => /^three/.test( p ),
 
-    output: {
+	output: {
 
-        name: 'URDFExporter',
-        extend: true,
-        format: 'umd',
-        file: outputPath,
-        sourcemap: true,
+		name: 'URDFExporter',
+		extend: true,
+		format: 'umd',
+		file: path.join( __dirname, './build/index.umd.cjs' ),
+		sourcemap: true,
 
-        globals: path => /^three/.test(path) ? 'THREE' : null,
+		globals: path => /^three/.test( path ) ? 'THREE' : null,
 
-    },
+	},
 
-};
+}, {
+
+	input: inputPath,
+	treeshake: false,
+	external: p => /^three/.test( p ),
+
+	output: {
+
+		name: 'URDFExporter',
+		extend: true,
+		format: 'es',
+		file: path.join( __dirname, './build/index.module.js' ),
+		sourcemap: true,
+
+		globals: path => /^three/.test( path ) ? 'THREE' : null,
+
+	},
+
+} ];
